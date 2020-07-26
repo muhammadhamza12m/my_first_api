db27c8368e543b4c5099370311ae0a3fbf6d1274
Usman Akram <usman.akram@gmail.com>
2020-07-26 16:21:37 +0500

    Update questions.js

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -11,6 +11,11 @@ router.get("/:id", async (req, res) => {
   let q = await question.findById(req.params.id);
   res.send(q);
 });
+router.post("/", async (req, res) => {
+  let q = new question(req.body);
+  await q.save();
+  res.send(q);
+});
 router.delete("/:id", async (req, res) => {
   let q = await question.findByIdAndDelete(req.params.id);
   res.send(q);
