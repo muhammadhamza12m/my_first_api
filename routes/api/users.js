2e9c8aa65887818f2e22a6fd547e095750fb95d9
muhammadhamza <muhammadhamza.12.m@gmail.com>
2020-07-27 15:43:54 +0500

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,10 +3,6 @@ let router = express.Router();
 let User  = require("../../models/user");
 
 router.post("/register", async (req, res) => {
-    let user = new User();
-  user.name = req.body.name;
-  user.email = req.body.email;
-    user.password = req.body.password;
    
 });
 module.exports = router;
