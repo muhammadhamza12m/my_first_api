a6129be601672632ab4beb6e4efa06d7a8fb74c6
muhammadhamza <muhammadhamza.12.m@gmail.com>
2020-07-27 21:35:55 +0500

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,13 +19,13 @@ router.post("/register", async (req, res) => {
 
 
 router.post("/login", async (req, res) => {
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("User Not Registered");
-  let isValid = await bcrypt.compare(req.body.password, user.password);
-  if (!isValid) return res.status(401).send("Invalid Password");
-  let token = jwt.sign(
-    { _id: user._id, name: user.name },"someprivatekey"
-  );
-  res.send(token);
+//   let user = await User.findOne({ email: req.body.email });
+//   if (!user) return res.status(400).send("User Not Registered");
+//   let isValid = await bcrypt.compare(req.body.password, user.password);
+//   if (!isValid) return res.status(401).send("Invalid Password");
+//   let token = jwt.sign(
+//     { _id: user._id, name: user.name },"someprivatekey"
+//   );
+//   res.send(token);
 });
 module.exports = router;
