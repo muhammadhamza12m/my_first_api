0abd0cd12cbb96509d3cd42f747806b057bf8ab0
muhammadhamza <muhammadhamza.12.m@gmail.com>
2020-07-27 14:59:21 +0500

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,9 +3,7 @@ let router = express.Router();
 let User  = require("../../models/user");
 
 router.post("/register", async (req, res) => {
-  let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User with given Email already exist");
-  user = new User();
+    let user = new User();
   user.name = req.body.name;
   user.email = req.body.email;
     user.password = req.body.password;
