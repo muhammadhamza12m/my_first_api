ce1e9673a80b43d194060942672a64f765c4a988
muhammadhamza <muhammadhamza.12.m@gmail.com>
2020-07-27 19:54:24 +0500

    log

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,31 @@
 var express = require('express');
 let router = express.Router();
-let {User}  = require("../../Models/user");
+let { User } = require("../../Models/user");
+var bcrypt = require("bcryptjs");
+const _ = require("lodash");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
-  let user = new User();
+    let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User with given Email already exist");
+  user = new User();
   user.name = req.body.name;
   user.email = req.body.email;
   user.password = req.body.password;
- // await user.generateHashedPassword();
+  await user.generateHashedPassword();
   await user.save();
-  return res.send(user);
+  return res.send(_.pick(user, ["name", "email"]));
+});
+
+
+outer.post("/login", async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("User Not Registered");
+  let isValid = await bcrypt.compare(req.body.password, user.password);
+  if (!isValid) return res.status(401).send("Invalid Password");
+  let token = jwt.sign(
+    { _id: user._id, name: user.name },"someprivatekey"
+  );
+  res.send(token);
 });
 module.exports = router;
